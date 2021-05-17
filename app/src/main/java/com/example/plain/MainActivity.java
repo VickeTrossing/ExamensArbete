7c1b4028a1d5248258652e10270eadadcc849219
VickeTrossing <vitro001@gmail.com>
2021-05-17 20:51:52 +0200

    Got BMI ot work. Working on second activity

--- app/src/main/java/com/example/plain/MainActivity.java
+++ app/src/main/java/com/example/plain/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.plain;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent = new Intent();
+    double bmi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void calculateCal(View view){
+    public void calculateCal(View view) {
 
+        EditText weightInput = findViewById(R.id.weightInput);
+        EditText heightInput = findViewById(R.id.heightInput);
         TextView output = findViewById(R.id.textView6);
 
-        output.setText("Ayy lmao");
+        double weight = Double.parseDouble(weightInput.getText().toString());
+        double height = Double.parseDouble(heightInput.getText().toString()) / 100;
 
+         bmi = weight / (height * height);
+
+        String bmiString = Double.toString(bmi);
+
+
+        if (output.getText() == "") {
+            output.setText(bmiString);
+        } else {
+            output.setText("");
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/plain/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.plain;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+    public void test(){
+
+
+    }
+}
