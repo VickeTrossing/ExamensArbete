c812d829ea9f06377860ba1adac0717fa6c7b03b
VickeTrossing <vitro001@gmail.com>
2021-05-17 18:45:48 +0200

    More design, added buttons and shit. Added some dummy code just to test stuff

--- app/src/main/java/com/example/plain/MainActivity.java
+++ app/src/main/java/com/example/plain/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.plain;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculateCal(View view){
+
+        TextView output = findViewById(R.id.textView6);
+
+        output.setText("Ayy lmao");
+
     }
 }
 
