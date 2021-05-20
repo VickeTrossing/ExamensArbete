990470e91b615fd2d2f27ecc8b906f33e65619f6
VickeTrossing <vitro001@gmail.com>
2021-05-20 07:48:29 +0200

    More design work on CalculateCal, also added items to a spinner

--- app/src/main/java/com/example/plain/CalculateCal.java
+++ app/src/main/java/com/example/plain/CalculateCal.java
@@ -1,14 +1,41 @@
 package com.example.plain;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CalculateCal extends AppCompatActivity {
+public class CalculateCal extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_cal);
+
+        Spinner spinner = findViewById(R.id.activitySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.acticity, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+
+    public void calculateCal(View view){
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
