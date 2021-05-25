37af85c64ee9466d3c062285fada8584bfc97e93
VickeTrossing <vitro001@gmail.com>
2021-05-25 16:08:51 +0200

    Changes the design of the home screen, looks better now. Might change all the design after this one

--- app/src/main/java/com/example/plain/CalculateBmi.java
+++ app/src/main/java/com/example/plain/CalculateBmi.java
@@ -32,7 +32,9 @@ public class CalculateBmi extends AppCompatActivity {
 
                 builder.setCancelable(true);
                 builder.setTitle("What is BMI?");
-                builder.setMessage("Body Mass Index (BMI) is a person's weight in kilograms divided by the square of height in meters. A high BMI can be an indicator of high body fatness. BMI can be used to screen for weight categories that may lead to health problems but it is not diagnostic of the body fatness or health of an individual.");
+                builder.setMessage("Body Mass Index (BMI) is a person's weight in kilograms divided by the square of height in meters. " +
+                        "A high BMI can be an indicator of high body fatness. BMI can be used to screen for weight categories that may " +
+                        "lead to health problems but it is not diagnostic of the body fatness or health of an individual.");
 
                 builder.setPositiveButton("Got it", new DialogInterface.OnClickListener() {
                     @Override
